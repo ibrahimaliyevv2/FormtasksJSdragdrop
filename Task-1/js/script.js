92a78220db702b7239b16e7bd45bfb5fc88c6710
ibrahimaliyevv2 <7cauc3b@code.edu.az>
2022-05-19 19:44:58 +0400

    formtasks added

---
+++ Task-1/js/script.js
@@ -0,0 +1,30 @@
+let fileInput = document.getElementById('fileInput');
+let uploadImg = document.getElementById("upload-image")
+
+fileInput.addEventListener('change', function(){
+    [...this.files].forEach(file=>{
+
+        if(file.type.startsWith('image/')){
+            let reader = new FileReader();
+
+            reader.onload = function(){
+
+                let img = document.createElement('img');
+                img.classList.add("new-img");
+                img.setAttribute('src',reader.result);
+
+                document.body.appendChild(img);
+            }
+
+            reader.readAsDataURL(file);
+        }
+        else{
+            console.log('Format is wrong');
+            this.value = '';
+        }
+    })
+})
+
+uploadImg.addEventListener('click', function(){
+    fileInput.click();
+})
